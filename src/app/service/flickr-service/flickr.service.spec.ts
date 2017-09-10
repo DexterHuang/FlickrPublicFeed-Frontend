6befebd73ff69b944199afcc2e0e3fc06fa38095
DexterHuang <ching.huang.dexter@gmail.com>
2017-09-10 21:29:43 +0800

    change to access Flickr directly from client site

--- src/app/service/flickr-service/flickr.service.spec.ts
+++ src/app/service/flickr-service/flickr.service.spec.ts
@@ -12,4 +12,8 @@ describe('FlickrService', () => {
   it('should be created', inject([FlickrService], (service: FlickrService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should build search query string', inject([FlickrService], (service: FlickrService) => {
+    expect(service.getURL('car cat')).toContain('&&tags=car,cat');
+  }))
 });
--- src/app/service/flickr-service/flickr.service.ts
+++ src/app/service/flickr-service/flickr.service.ts
@@ -1,28 +1,63 @@
 import { ImageList } from './../../Model/ImageList';
-import { Http } from '@angular/http';
+import { Http, Jsonp } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FlickrService {
 
-  imageList: ImageList;
-  loading = true;
-  constructor(private http: Http) {
+  private imageList: ImageList;
+  private loading = true;
+  constructor(private http: Http, private jsonp: Jsonp) {
     this.refreshFeed()
   }
 
   // Feed is downloaded and chached
-  public refreshFeed() {
+  public refreshFeed(searchText = '') {
     this.loading = true;
-    this.http.get('http://localhost:3000/api/v1/feeds').subscribe(r => {
-      const jsonObject = r.json();
+    // Making JSONP request directly to Flickr
+    this.jsonp.request(this.getURL(searchText), {
+      method: 'GET'
+    }).subscribe(r => {
+      const jsonObject = r.json()
       const imageList = new ImageList();
       Object.assign(imageList, jsonObject);
       this.imageList = imageList;
       this.loading = false;
     })
   }
+  public getURL(searchText = '') {
+    const base = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&&jsoncallback=JSONP_CALLBACK';
+    if (searchText.length > 0) {
+      return base + this.buildSearchQuery(searchText);
+    }
+    return base;
+  }
+  private buildSearchQuery(searchText: string) {
+    searchText = searchText.trim();
+    while (searchText.indexOf(' ') >= 0) {
+      searchText = searchText.replace(' ', ',');
+    }
+    return '&&tags=' + searchText;
+  }
   public getFeed(): ImageList {
     return this.imageList;
+  }
+  public isLoading() {
+    return this.loading;
+  }
+  // Escape Unwanted chracters
+  public cleanString(str: string) {
+    let string = str;
+    string = string.replace(/\\n/g, '\\n')
+      .replace(/\\'/g, `\\\'`)
+      .replace(/\\"/g, '\\"')
+      .replace(/\\&/g, '\\&')
+      .replace(/\\r/g, '\\r')
+      .replace(/\\t/g, '\\t')
+      .replace(/\\b/g, '\\b')
+      .replace(/\\f/g, '\\f');
+    // remove non-printable and other non-valid JSON chars
+    string = string.replace(/[\u0000-\u0019]+/g, '');
+    return string;
   }
 }
