d56bdd30cefe2f96c4ff17b8d17a6b7a1f8c3a37
DexterHuang <ching.huang.dexter@gmail.com>
2017-09-10 21:30:50 +0800

    added search function

--- src/app/Model/ImageList.ts
+++ src/app/Model/ImageList.ts
@@ -5,5 +5,20 @@ export class ImageList {
     description: string;
     modified: Date;
     generator: string;
-    items: Image[]
+    private items: any[] = []
+
+    getImages() {
+        if (this.items && this.items.length > 0) {
+            if (this.items[0] instanceof Image === false) {
+                const list: Image[] = [];
+                this.items.forEach(obj => {
+                    const img = new Image();
+                    Object.assign(img, obj);
+                    list.push(img);
+                })
+                this.items = list;
+            }
+        }
+        return this.items;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { FlickrService } from './service/flickr-service/flickr.service';
-import { HttpModule } from '@angular/http';
+import { HttpModule, JsonpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page/home-page.component';
@@ -10,6 +10,7 @@ import { NavbarComponent } from './common/navbar/navbar.component';
 import { ImageListComponent } from './common/image-list/image-list.component';
 import { ImageListItemComponent } from './common/image-list-item/image-list-item.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchBarComponent } from './common/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -18,12 +19,15 @@ import { HttpClientModule } from '@angular/common/http';
     NavbarComponent,
     ImageListComponent,
     ImageListItemComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     HttpModule,
+    JsonpModule,
+    FormsModule,
   ],
   providers: [FlickrService],
   bootstrap: [AppComponent]
