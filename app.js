36c9791737dda5f747adf131dd091186c0b936b4
DexterHuang <ching.huang.dexter@gmail.com>
2017-09-12 00:37:01 +0800

    remove unneed files

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var feeds = require('./routes/feeds');
 var cors = require('cors');
 
 
@@ -13,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+//Hosting static Angualr app
 app.use(express.static(__dirname + '/dist'));
 
 module.exports = app;
